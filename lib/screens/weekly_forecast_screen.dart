a24b19f99f1ca11e05ea7614397bef70bf5f0a6c
mozesnjoroge <njorogemosesngure@gmail.com>
2022-02-18 12:00:12 +0300

    Constructing weekly forecast screen

--- lib/screens/weekly_forecast_screen.dart
+++ lib/screens/weekly_forecast_screen.dart
@@ -5,7 +5,8 @@ class WeeklyForecastScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int forecastItems = 7;
+    int todayForecastItems = 10;
+    int weeklyForecastItems = 7;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0XFF080A33),
@@ -17,99 +18,184 @@ class WeeklyForecastScreen extends StatelessWidget {
       body: Container(
         color: const Color(0xFF080A33),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: [
-                  // ignore: prefer_const_constructors
-                  Text(
-                    'Today',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
-                  ),
-                  // ignore: prefer_const_constructors
-                  InkWell(
-                    onTap: () {
-                      //TODO : Open week forecast screen
-                    },
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
                     // ignore: prefer_const_constructors
-                    child: Text(
-                      'View full report',
+                    Text(
+                      'Today',
                       style: const TextStyle(
-                        fontSize: 12,
-                        color: Color(0xFF4286E8),
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 80,
-                child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: const BoxDecoration(
-                          color: Color(0xFF4286E6),
-                          shape: BoxShape.rectangle,
-                          // RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                          // ),
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            // ignore: prefer_const_constructors
-                            FlutterLogo(
-                              size: 50.0,
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              // ignore: prefer_const_literals_to_create_immutables
-                              children: [
-                                // ignore: prefer_const_constructors
-                                Text(
-                                  '14:00',
-                                  // ignore: prefer_const_constructors
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 12),
-                                  // ignore: prefer_const_constructors
-                                ),
-                                // ignore: prefer_const_constructors
-                                Text(
-                                  '32\u2103',
-                                  // ignore: prefer_const_constructors
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 18),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) {
+                    // ignore: prefer_const_constructors
+                    InkWell(
+                      onTap: () {
+                        //TODO : Open week forecast screen
+                      },
                       // ignore: prefer_const_constructors
-                      return SizedBox(
-                        width: 10,
-                      );
-                    },
-                    itemCount: forecastItems),
-              )
-            ],
+                      child: Text(
+                        '${DateTime.now()}', //TODO : Format this date to match design
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Color(0xFF4286E8),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                  child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF4286E6),
+                            shape: BoxShape.rectangle,
+                            // RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                            // ),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              // ignore: prefer_const_constructors
+                              FlutterLogo(
+                                size: 50.0,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  // ignore: prefer_const_constructors
+                                  Text(
+                                    '14:00',
+                                    // ignore: prefer_const_constructors
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 12),
+                                    // ignore: prefer_const_constructors
+                                  ),
+                                  // ignore: prefer_const_constructors
+                                  Text(
+                                    '32\u2103',
+                                    // ignore: prefer_const_constructors
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        // ignore: prefer_const_constructors
+                        return SizedBox(
+                          width: 10,
+                        );
+                      },
+                      itemCount: todayForecastItems),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.max,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    // ignore: prefer_const_constructors
+                    Text(
+                      'This week',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+
+                    // ignore: prefer_const_constructors
+                    Icon(
+                      Icons.calendar_today,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                Flexible(
+                  child: ListView.separated(
+                      itemBuilder: ((context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0XFF080A33),
+                            border: Border.all(
+                              width: 1.0,
+                              color: const Color(0xFF4286E6),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                // ignore: prefer_const_literals_to_create_immutables
+                                children: [
+                                  const Text(
+                                    'Temp',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const Text(
+                                    '32\u2103',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Text(
+                                '32\u2103',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 30,
+                                ),
+                              ),
+                              // ignore: prefer_const_constructors
+                              FlutterLogo(
+                                size: 40,
+                              )
+                            ],
+                          ),
+                        );
+                      }),
+                      separatorBuilder: (context, index) {
+                        // ignore: prefer_const_constructors
+                        return SizedBox(
+                          height: 10,
+                        );
+                      },
+                      itemCount: weeklyForecastItems),
+                )
+              ],
+            ),
           ),
         ),
       ),
