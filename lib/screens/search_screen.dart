7ff1fe4c72e3f351ec4187c582b8b8726f7ec2ac
mozesnjoroge <njorogemosesngure@gmail.com>
2022-02-21 20:05:21 +0300

    Refactored search screen widgets

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -90,9 +90,7 @@ class _SearchLocationScreenState extends State<SearchLocationScreen> {
 
                         decoration: BoxDecoration(
                           color: const Color(0xFF222249),
-                          // color: const Color(0XFF080A33),
                           borderRadius: BorderRadius.circular(5),
-                          // border: Border.all(width: 0.5, color: Colors.white),
                         ),
                         // ignore: prefer_const_constructors
                         child: Icon(
@@ -135,70 +133,81 @@ class _SearchLocationScreenState extends State<SearchLocationScreen> {
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-            decoration: BoxDecoration(
-                color: const Color(0xFF4286E6),
-                borderRadius: BorderRadius.circular(15),
-                shape: BoxShape.rectangle),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                  // ignore: prefer_const_literals_to_create_immutables
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: [
-                        const Text(
-                          '32\u2103',
-                          // ignore: unnecessary_const
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        const Text(
-                          'Cloudy',
-                          // ignore: unnecessary_const
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    // ignore: prefer_const_constructors
-                    SizedBox(
-                      width: 15,
-                    ),
-                    // ignore: prefer_const_constructors
-                    FlutterLogo(
-                      size: 35,
-                    ),
-                  ],
-                ),
-                // ignore: prefer_const_constructors
-                SizedBox(height: 20),
-                const Text(
-                  'Carlifornia',
-                  // ignore: unnecessary_const
-                  style: const TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          );
+          return LocationWeatherCard();
         });
+  }
+}
+
+class LocationWeatherCard extends StatelessWidget {
+  const LocationWeatherCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+      decoration: BoxDecoration(
+          color: const Color(0xFF4286E6),
+          borderRadius: BorderRadius.circular(15),
+          shape: BoxShape.rectangle),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Text(
+                    '32\u2103',
+                    // ignore: unnecessary_const
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Cloudy',
+                    // ignore: unnecessary_const
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              // ignore: prefer_const_constructors
+              SizedBox(
+                width: 15,
+              ),
+              // ignore: prefer_const_constructors
+              FlutterLogo(
+                size: 35,
+              ),
+            ],
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(height: 20),
+          const Text(
+            'Carlifornia',
+            // ignore: unnecessary_const
+            style: const TextStyle(
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'hourly_weather_card.dart';
 export 'daily_forecast_card.dart';
+export 'location_weather_card.dart';
