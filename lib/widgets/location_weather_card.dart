caf1167f6603060fcc01ab0799c734fbafcb4932
mozesnjoroge <njorogemosesngure@gmail.com>
2022-02-23 18:56:54 +0300

    Refactoring location weather card

--- lib/widgets/location_weather_card.dart
+++ lib/widgets/location_weather_card.dart
@@ -8,7 +8,8 @@ class LocationWeatherCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+      
+      margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
       padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
       decoration: BoxDecoration(
           color: const Color(0xFF4286E6),
@@ -19,14 +20,14 @@ class LocationWeatherCard extends StatelessWidget {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
+    
             // ignore: prefer_const_literals_to_create_immutables
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.max,
-
+    
                 // ignore: prefer_const_literals_to_create_immutables
                 children: [
                   const Text(
