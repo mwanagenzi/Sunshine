f6676864618d7f547639a3e3621150e53621d3d7
mozesnjoroge <njorogemosesngure@gmail.com>
2022-05-09 16:28:14 +0300

    Solved render flex error from keyboard pop up

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -29,146 +29,138 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              flex: 3,
-              child: SvgPicture.asset(
+      body: SingleChildScrollView(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SvgPicture.asset(
                 'assets/images/curve_left.svg',
                 color: Palette.activeCardColor,
                 width: 300,
               ),
-            ),
-            const SizedBox(height: 10),
-            Flexible(
-              flex: 5,
-              child: Padding(
+              const SizedBox(height: 10),
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Palette.highlightedTextColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(10),
+                child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Center(
+                        child: Text(
+                          'Hi!',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 2),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        controller: _emailController,
+                        cursorColor: Colors.black,
+                        autofocus: true,
+                        decoration: InputDecoration(
+                            focusColor: Palette.highlightedTextColor,
+                            hintText: 'Email Address',
+                            // ignore: prefer_const_constructors
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                            ),
+                            prefixIcon: const Icon(
+                              Icons.search_rounded,
+                              color: Colors.white,
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              borderSide: const BorderSide(color: Colors.white),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white),
+                      ),
+                      const SizedBox(height: 20),
+                      SunshineAuthButton(
+                        buttonText: 'Continue',
+                        buttonFunction: () {
+                          //TODO: check whether user exists or not
+                          //TODO:navigate to relevant screen (login or register)
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Center(
+                        child: Text('or',
+                            style: Theme.of(context).textTheme.bodyText1),
+                      ),
+                      const SizedBox(height: 20),
+                      ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        tileColor: Colors.white,
+                        leading: const Icon(Icons.facebook_rounded),
+                        title: const Text('Continue with Facebook'),
+                        onTap: () {
+                          //TODO Open Facebook account api
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        tileColor: Colors.white,
+                        leading: const Icon(Icons.search_outlined),
+                        title: const Text('Continue with Google'),
+                        onTap: () {
+                          //TODO Open Google account api
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
                         children: [
-                          Center(
-                            child: Text(
-                              'Hi!',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline5!
-                                  .copyWith(
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: 2),
-                            ),
-                          ),
-                          TextFormField(
-                            controller: _emailController,
-                            cursorColor: Colors.black,
-                            autofocus: true,
-                            decoration: InputDecoration(
-                                focusColor: Palette.highlightedTextColor,
-                                hintText: 'Email Address',
-                                // ignore: prefer_const_constructors
-                                hintStyle: TextStyle(
-                                  color: Colors.black,
-                                ),
-                                prefixIcon: const Icon(
-                                  Icons.search_rounded,
-                                  color: Colors.white,
-                                ),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                  borderSide:
-                                      const BorderSide(color: Colors.white),
-                                ),
-                                filled: true,
-                                fillColor: Colors.white),
-                          ),
-                          SunshineAuthButton(
-                            buttonText: 'Continue',
-                            buttonFunction: () {
-                              //TODO: check whether user exists or not
-                              //TODO:navigate to relevant screen (login or register)
-                            },
-                          ),
-                          Center(
-                            child: Text('or',
-                                style: Theme.of(context).textTheme.bodyText1),
-                          ),
-                          ListTile(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            tileColor: Colors.white,
-                            leading: const Icon(Icons.facebook_rounded),
-                            title: const Text('Continue with Facebook'),
-                            onTap: () {
-                              //TODO Open Facebook account api
-                            },
-                          ),
-                          ListTile(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            tileColor: Colors.white,
-                            leading: const Icon(Icons.search_outlined),
-                            title: const Text('Continue with Google'),
-                            onTap: () {
-                              //TODO Open Google account api
-                            },
-                          ),
-                          Row(
-                            children: [
-                              const Text("Don't have an account?"),
-                              const SizedBox(width: 10),
-                              InkWell(
-                                onTap: () {
-                                  //TODO navigate to the registration page
-                                },
-                                child: const Text(
-                                  'Sign Up',
-                                  style: TextStyle(
-                                      color: Palette.highlightedTextColor),
-                                ),
-                              ),
-                            ],
-                          ),
+                          const Text("Don't have an account?"),
+                          const SizedBox(width: 10),
                           InkWell(
                             onTap: () {
-                              //TODO: password reset functionality
+                              //TODO navigate to the registration page
                             },
-                            child: Text(
-                              'Forgot your password?',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText2!
-                                  .copyWith(
-                                      color: Palette.highlightedTextColor),
-                              //
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  color: Palette.highlightedTextColor),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 20),
+                      InkWell(
+                        onTap: () {
+                          //TODO: password reset functionality
+                        },
+                        child: Text(
+                          'Forgot your password?',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText2!
+                              .copyWith(color: Palette.highlightedTextColor),
+                          //
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-          ],
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
