c31d1ac8b0e94d0a95cb7ef5c5eaffcef5f1f10f
mozesnjoroge <njorogemosesngure@gmail.com>
2022-05-10 23:03:22 +0300

    Configring application routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sunshine/provider/navbar_tab_manager.dart';
 import 'package:sunshine/screens/screens.dart';
 import 'package:sunshine/sunshine_theme/theme.dart';
 
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,14 @@
+class AppRoutes {
+  static const home = '/home';
+  static const location = '/search/location';
+  static const login = '/login';
+  static const resetPassword = '/resetPasssword';
+  static const search = '/search';
+  static const signUp = '/signUp';
+  static const weeklyForecast = '/weeklyForecast';
+  static const welcome = '/welcome';
+}
+
 const String kApiKey = '1cf2f5276d1b444093434825221204';
 const String kWeatherApiUrl = 'https://api.weatherapi.com/v1';
-
 const String kForecastApiMethod = '/forecast.json';
