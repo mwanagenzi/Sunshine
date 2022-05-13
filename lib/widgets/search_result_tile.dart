3e3c5b5049171fc0d604f71ee74e3e527b76b7b0
mozesnjoroge <njorogemosesngure@gmail.com>
2022-05-13 16:42:23 +0300

    Working on search result model

--- lib/widgets/search_result_tile.dart
+++ lib/widgets/search_result_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sunshine/sunshine_theme/theme.dart';
 
 class SearchResultTile extends StatelessWidget {
   final String searchName;
@@ -15,6 +16,15 @@ class SearchResultTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: (() {
+        //TODO; Open current weaher screen with relevant weather data
+      }),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: const BorderSide(
+          color: Palette.activeCardColor,
+        ),
+      ),
       title: Text('Name : $searchName '),
       subtitle: Text('Region : $searchRegion'),
       trailing: Text('Country : $searchCountry'),
